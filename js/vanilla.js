0cffc7409408b2c3f7de5253a46187b65cdc829d
ong7 <ondig7@gmail.com>
2019-10-21 15:09:52 +0200

    feature

--- js/vanilla.js
+++ js/vanilla.js
@@ -2,10 +2,10 @@
 
 let mainContainer = document.getElementById("mainContainer");
 mainContainer.classList.add('clearfix');
-var positionZero = 1;
+let positionZero = 1;
 
 
-let imgObjt = [ {name:"bryan-minear", path: "img\/bryan-minear-VmMHIRRgQpY-unsplash.jpg"}, 
+const imgObjt = [ {name:"bryan-minear", path: "img\/bryan-minear-VmMHIRRgQpY-unsplash.jpg"}, 
                 {name:"eva-blue", path: "img\/eva-blue-SfPOkp6-2eA-unsplash.jpg"},
                 {name:"finding-dan-dan-grinwis", path: "img\/finding-dan-dan-grinwis-O35rT6OytRo-unsplash.jpg"},
                 {name:"harshil-gudka", path: "img\/harshil-gudka-ORcQipPJAy4-unsplash.jpg"},
@@ -13,7 +13,7 @@ let imgObjt = [ {name:"bryan-minear", path: "img\/bryan-minear-VmMHIRRgQpY-unspl
                 {name:"paul-fiedler", path: "img\/paul-fiedler-q1QPK60bv1g-unsplash.jpg"},
                 {name:"pawel-nolbert", path: "img\/pawel-nolbert-4u2U8EO9OzY-unsplash.jpg"},
                 {name:"sutirta-budiman", path: "img\/sutirta-budiman-kjOBqwMUnWw-unsplash.jpg"}]
-let imgString = '[{"name":"bryan-minear","path":"img\/bryan-minear-VmMHIRRgQpY-unsplash.jpg"},{"name":"eva-blue","path":"img\/eva-blue-SfPOkp6-2eA-unsplash.jpg"},{"name":"finding-dan-dan-grinwis","path":"img\/finding-dan-dan-grinwis-O35rT6OytRo-unsplash.jpg"},{"name":"harshil-gudka","path":"img\/harshil-gudka-ORcQipPJAy4-unsplash.jpg"},{"name":"max-bender","path":"img\/max-bender-VmX3vmBecFE-unsplash.jpg"},{"name":"paul-fiedler","path":"img\/paul-fiedler-q1QPK60bv1g-unsplash.jpg"},{"name":"pawel-nolbert","path":"img\/pawel-nolbert-4u2U8EO9OzY-unsplash.jpg"},{"name":"sutirta-budiman","path":"img\/sutirta-budiman-kjOBqwMUnWw-unsplash.jpg"}]'
+const imgString = '[{"name":"bryan-minear","path":"img\/bryan-minear-VmMHIRRgQpY-unsplash.jpg"},{"name":"eva-blue","path":"img\/eva-blue-SfPOkp6-2eA-unsplash.jpg"},{"name":"finding-dan-dan-grinwis","path":"img\/finding-dan-dan-grinwis-O35rT6OytRo-unsplash.jpg"},{"name":"harshil-gudka","path":"img\/harshil-gudka-ORcQipPJAy4-unsplash.jpg"},{"name":"max-bender","path":"img\/max-bender-VmX3vmBecFE-unsplash.jpg"},{"name":"paul-fiedler","path":"img\/paul-fiedler-q1QPK60bv1g-unsplash.jpg"},{"name":"pawel-nolbert","path":"img\/pawel-nolbert-4u2U8EO9OzY-unsplash.jpg"},{"name":"sutirta-budiman","path":"img\/sutirta-budiman-kjOBqwMUnWw-unsplash.jpg"}]'
     
 
 
@@ -51,37 +51,41 @@ function addImgObj(imObj){
 
 addImg(imgObjt);
 
-let left = document.querySelector(".apres");
-let right = document.querySelector(".avant");
+let right = document.querySelector(".right");
+let left = document.querySelector(".left");
 
 let showSlides = function(n) {
-    var i;
-    var slides = document.getElementsByClassName("containerImg");
+    let i = null;
+    const slides = document.getElementsByClassName("containerImg");
     
     if (n > slides.length) {
-        positionZero = 1
+        positionZero = 1;
+       
     }
-    if (n < 1) {
-        positionZero = slides.length
+    else if (n < 1) {
+        positionZero = slides.length;
+       
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
+
     slides[positionZero-1].style.display = "block";
+    
     
   } 
 
 showSlides(positionZero);
 
 
-let next = function () {
+const next = function () {
   showSlides(positionZero += 1);
 }
 
-let previous = function(){
-    showSlides(positionZero = positionZero - 1);
+const previous = function(){
+    showSlides(positionZero -= 1);
 }
 
-left.addEventListener('click', next);
-right.addEventListener('click', previous);
+right.addEventListener('click', next);
+left.addEventListener('click', previous);
